2eb44e8b710c86cae7cff10d135ba68e04f8e0f5
NGANDJUI HANGA IVAN DELORS <ngandjui.ivan@institutsaintjean.org>
2025-04-12 11:13:44 +0100

    Implementation du squelette de la section 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavBarComponent} from './composants/nav-bar/nav-bar.component';
 import {FooterComponent} from './composants/footer/footer.component';
+import {Section1Component} from './main/section1/section1.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent, FooterComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, Section1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
