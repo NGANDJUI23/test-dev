0721238dce892d4473ccd63854edb6b167d2ffb0
NGANDJUI HANGA IVAN DELORS <ngandjui.ivan@institutsaintjean.org>
2025-04-12 09:48:40 +0100

    Implementation du squelette(HTML) du footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavBarComponent} from './composants/nav-bar/nav-bar.component';
+import {FooterComponent} from './composants/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
